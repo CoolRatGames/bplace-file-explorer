2fd6b26a74674e18b935a9c0eacacf1d9bcfafa6
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2026-03-19 10:12:43 +0100

    Image Size: Dynamic

--- src/template/TemplateImageView.tsx
+++ src/template/TemplateImageView.tsx
@@ -2,7 +2,7 @@ import type {TemplateFile} from "./Types.ts";
 import styled from "styled-components";
 
 const Container = styled.img`
-    width: 50vw;
+    width: clamp(250px, 25vw, 600px);
     outline: 2px solid ${({theme}) => theme.colors.border1};
     outline-offset: 2px;
     image-rendering: pixelated;
